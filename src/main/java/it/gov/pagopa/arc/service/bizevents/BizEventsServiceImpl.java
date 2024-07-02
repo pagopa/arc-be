87e85d620cfd915be197b2e5726bb6edcea5bb03
Giuseppe-LaManna <127106061+Giuseppe-LaManna@users.noreply.github.com>
2024-07-02 11:29:46 +0200

    feat: P4PU-201 added business logic to retrieve transaction details (#17)
    * P4PU-201 added business logic to retrieve transaction details
    
    * P4PU-201 added fakers
    
    * P4PU-201 added tests
    
    * P4PU-201 updated service
    
    * P4PU-201 updated mapper and tests

--- src/main/java/it/gov/pagopa/arc/service/bizevents/BizEventsServiceImpl.java
+++ src/main/java/it/gov/pagopa/arc/service/bizevents/BizEventsServiceImpl.java
@@ -1,11 +1,15 @@
 package it.gov.pagopa.arc.service.bizevents;
 
 import it.gov.pagopa.arc.connector.bizevents.BizEventsConnector;
+import it.gov.pagopa.arc.connector.bizevents.dto.BizEventsTransactionDetailsDTO;
 import it.gov.pagopa.arc.connector.bizevents.dto.BizEventsTransactionsListDTO;
 import it.gov.pagopa.arc.dto.mapper.BizEventsTransactionDTO2TransactionDTO;
+import it.gov.pagopa.arc.dto.mapper.BizEventsTransactionDetails2TransactionDetailsDTO;
 import it.gov.pagopa.arc.dto.mapper.BizEventsTransactionsListDTO2TransactionsListDTO;
 import it.gov.pagopa.arc.model.generated.TransactionDTO;
+import it.gov.pagopa.arc.model.generated.TransactionDetailsDTO;
 import it.gov.pagopa.arc.model.generated.TransactionsListDTO;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
@@ -13,21 +17,15 @@ import java.util.ArrayList;
 import java.util.List;
 
 @Service
+@RequiredArgsConstructor
 public class BizEventsServiceImpl implements BizEventsService{
+
+    @Value("${rest-client.biz-events.fake-fiscal-code}")
     private final String fakeFiscalCode;
     private final BizEventsConnector bizEventsConnector;
     private final BizEventsTransactionDTO2TransactionDTO transactionDTOMapper;
     private final BizEventsTransactionsListDTO2TransactionsListDTO transactionsListDTOMapper;
-
-    public BizEventsServiceImpl(@Value("${rest-client.biz-events.fake-fiscal-code}") String fakeFiscalCode,
-                                BizEventsConnector bizEventsConnector,
-                                BizEventsTransactionDTO2TransactionDTO transactionDTOMapper,
-                                BizEventsTransactionsListDTO2TransactionsListDTO transactionsListDTOMapper) {
-        this.fakeFiscalCode = fakeFiscalCode;
-        this.bizEventsConnector = bizEventsConnector;
-        this.transactionDTOMapper = transactionDTOMapper;
-        this.transactionsListDTOMapper = transactionsListDTOMapper;
-    }
+    private final BizEventsTransactionDetails2TransactionDetailsDTO transactionDetailsDTOMapper;
 
     @Override
     public TransactionsListDTO retrieveTransactionsListFromBizEvents(Integer page, Integer size, String filter) {
@@ -43,5 +41,11 @@ public class BizEventsServiceImpl implements BizEventsService{
             transactions = new ArrayList<>();
         }
         return transactionsListDTOMapper.apply(transactions, size);
+    }
+
+    @Override
+    public TransactionDetailsDTO retrieveTransactionDetailsFromBizEvents(String transactionId) {
+        BizEventsTransactionDetailsDTO bizEventsTransactionDetails = bizEventsConnector.getTransactionDetails(fakeFiscalCode, transactionId);
+        return transactionDetailsDTOMapper.apply(bizEventsTransactionDetails);
     }
 }
--- src/test/java/it/gov/pagopa/arc/dto/mapper/BizEventsTransactionDetails2TransactionDetailsDTOTest.java
+++ src/test/java/it/gov/pagopa/arc/dto/mapper/BizEventsTransactionDetails2TransactionDetailsDTOTest.java
@@ -1,13 +1,9 @@
 package it.gov.pagopa.arc.dto.mapper;
 
-import it.gov.pagopa.arc.connector.bizevents.dto.*;
-import it.gov.pagopa.arc.fakers.CartItemDTOFaker;
-import it.gov.pagopa.arc.fakers.CommonUserDetailDTOFaker;
-import it.gov.pagopa.arc.fakers.CommonWalletInfoDTOFaker;
-import it.gov.pagopa.arc.fakers.InfoTransactionDTOFaker;
-import it.gov.pagopa.arc.fakers.bizEvents.BizEventsCartItemDTOFaker;
-import it.gov.pagopa.arc.fakers.bizEvents.BizEventsInfoTransactionDTOFaker;
-import it.gov.pagopa.arc.model.generated.*;
+import it.gov.pagopa.arc.connector.bizevents.dto.BizEventsTransactionDetailsDTO;
+import it.gov.pagopa.arc.fakers.TransactionDetailsDTOFaker;
+import it.gov.pagopa.arc.fakers.bizEvents.BizEventsTransactionDetailsDTOFaker;
+import it.gov.pagopa.arc.model.generated.TransactionDetailsDTO;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -15,9 +11,8 @@ import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
 
-import java.util.List;
-
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.mockito.ArgumentMatchers.any;
 
 @ExtendWith(MockitoExtension.class)
@@ -27,53 +22,34 @@ class BizEventsTransactionDetails2TransactionDetailsDTOTest {
 
     @Mock
     private BizEventsInfoTransaction2InfoTransactionDTO transactionInfoMapperMock;
+    @Mock
+    private BizEventsCartItem2CartItemDTO cartItemDTOMapperMock;
 
     @BeforeEach
     void setUp() {
-        transactionDetailsMapper = new BizEventsTransactionDetails2TransactionDetailsDTO(transactionInfoMapperMock);
+        transactionDetailsMapper = new BizEventsTransactionDetails2TransactionDetailsDTO(transactionInfoMapperMock, cartItemDTOMapperMock);
     }
 
     @Test
     void givenBizEventsTransactionDetailsWhenApplyThenReturnTransactionDetails() {
         //given
         //create BizEventsTransactionDetailsDTO
-        BizEventsWalletInfoDTO bizEventsWalletInfo = CommonWalletInfoDTOFaker.mockBizEventsWalletInfoDTO(false);
-        BizEventsUserDetailDTO payer = CommonUserDetailDTOFaker.mockBizEventsUserDetailDTO(CommonUserDetailDTOFaker.USER_DETAIL_PAYER);
-        BizEventsInfoTransactionDTO bizEventsInfoTransaction = BizEventsInfoTransactionDTOFaker.mockInstance(bizEventsWalletInfo, payer);
-
-        BizEventsUserDetailDTO payee = CommonUserDetailDTOFaker.mockBizEventsUserDetailDTO(CommonUserDetailDTOFaker.USER_DETAIL_PAYEE);
-        BizEventsUserDetailDTO debtor = CommonUserDetailDTOFaker.mockBizEventsUserDetailDTO(CommonUserDetailDTOFaker.USER_DETAIL_DEBTOR);
-        BizEventsCartItemDTO bizEventsCartItem1 = BizEventsCartItemDTOFaker.mockInstance(payee,debtor);
-        BizEventsCartItemDTO bizEventsCartItem2 = BizEventsCartItemDTOFaker.mockInstance(payee,debtor);
-        List<BizEventsCartItemDTO> bizEventsCartsList = List.of(bizEventsCartItem1, bizEventsCartItem2);
-
-        BizEventsTransactionDetailsDTO bizEventsTransactionDetailsDTO = BizEventsTransactionDetailsDTO.builder()
-                .bizEventsInfoTransactionDTO(bizEventsInfoTransaction)
-                .bizEventsCartsDTO(bizEventsCartsList)
-                .build();
-
+        BizEventsTransactionDetailsDTO bizEventsTransactionDetailsDTO = BizEventsTransactionDetailsDTOFaker.mockInstance();
 
         //create TransactionDetailsDTO
-        WalletInfoDTO walletInfo = CommonWalletInfoDTOFaker.mockWalletInfoDTO(false);
-        UserDetailDTO payerMapped = CommonUserDetailDTOFaker.mockUserDetailDTO(CommonUserDetailDTOFaker.USER_DETAIL_PAYER);
-        InfoTransactionDTO infoTransaction = InfoTransactionDTOFaker.mockInstance(walletInfo, payerMapped);
+        TransactionDetailsDTO transactionDetailsDTO = TransactionDetailsDTOFaker.mockInstance();
 
-        UserDetailDTO payeeResponse = CommonUserDetailDTOFaker.mockUserDetailDTO(CommonUserDetailDTOFaker.USER_DETAIL_PAYEE);
-        UserDetailDTO debtorResponse = CommonUserDetailDTOFaker.mockUserDetailDTO(CommonUserDetailDTOFaker.USER_DETAIL_DEBTOR);
-        CartItemDTO cartItem1 = CartItemDTOFaker.mockInstance(payeeResponse, debtorResponse);
-        CartItemDTO cartItem2 = CartItemDTOFaker.mockInstance(payeeResponse, debtorResponse);
-        List<CartItemDTO> cartsList = List.of(cartItem1, cartItem2);
-
-        Mockito.when(transactionInfoMapperMock.mapInfoTransaction(bizEventsInfoTransaction)).thenReturn(infoTransaction);
+        Mockito.when(transactionInfoMapperMock.mapInfoTransaction(bizEventsTransactionDetailsDTO.getBizEventsInfoTransactionDTO())).thenReturn(transactionDetailsDTO.getInfoTransaction());
+        Mockito.when(cartItemDTOMapperMock.mapCart(bizEventsTransactionDetailsDTO.getBizEventsCartsDTO().get(0))).thenReturn(transactionDetailsDTO.getCarts().get(0));
+        Mockito.when(cartItemDTOMapperMock.mapCart(bizEventsTransactionDetailsDTO.getBizEventsCartsDTO().get(1))).thenReturn(transactionDetailsDTO.getCarts().get(1));
         //when
-        TransactionDetailsDTO result = transactionDetailsMapper.apply(bizEventsTransactionDetailsDTO, cartsList);
+        TransactionDetailsDTO result = transactionDetailsMapper.apply(bizEventsTransactionDetailsDTO);
         //then
         assertNotNull(result);
-        assertEquals(infoTransaction, result.getInfoTransaction());
-        System.out.println(infoTransaction.getWalletInfo());
-        System.out.println(result.getInfoTransaction().getWalletInfo());
+        assertEquals(transactionDetailsDTO.getInfoTransaction(), result.getInfoTransaction());
         assertEquals(2, result.getCarts().size());
-        assertEquals( cartsList, result.getCarts());
+        assertEquals( transactionDetailsDTO.getCarts(), result.getCarts());
         Mockito.verify(transactionInfoMapperMock).mapInfoTransaction(any());
+        Mockito.verify(cartItemDTOMapperMock,Mockito.times(2)).mapCart(any());
     }
 }
