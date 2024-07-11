fd1fdf0214571e6cbf798afda255abc0725b3478
Giuseppe-La-Manna <Giuseppe.LaManna@emeal.nttdata.com>
2024-07-11 17:55:15 +0200

    P4PU-243 renamed BizEventsNotFoundException.java to BizEventsTransactionNotFoundException and added new BizEventsReceiptNotFoundException

--- src/main/java/it/gov/pagopa/arc/exception/custom/BizEventsNotFoundException.java
+++
@@ -1,8 +0,0 @@
-package it.gov.pagopa.arc.exception.custom;
-
-import lombok.Getter;
-
-@Getter
-public class BizEventsNotFoundException extends RuntimeException {
-    public BizEventsNotFoundException(String message){super(message);}
-}
