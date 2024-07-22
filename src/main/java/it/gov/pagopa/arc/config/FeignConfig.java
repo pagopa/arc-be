077e19b26be37f990db9e00211782a0193bb8d80
Giuseppe-La-Manna <Giuseppe.LaManna@emeal.nttdata.com>
2024-07-22 16:54:09 +0200

    P4PU-283 added pull payment Feign client

--- src/main/java/it/gov/pagopa/arc/config/FeignConfig.java
+++ src/main/java/it/gov/pagopa/arc/config/FeignConfig.java
@@ -1,10 +1,11 @@
 package it.gov.pagopa.arc.config;
 
 import it.gov.pagopa.arc.connector.bizevents.BizEventsRestClient;
+import it.gov.pagopa.arc.connector.pullpayment.PullPaymentRestClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-@EnableFeignClients(clients = {BizEventsRestClient.class})
+@EnableFeignClients(clients = {BizEventsRestClient.class, PullPaymentRestClient.class})
 public class FeignConfig {
 }
