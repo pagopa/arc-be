8ef5b2e3664f572495bc215169c9e9573c70b846
Giuseppe-La-Manna <Giuseppe.LaManna@emeal.nttdata.com>
2024-07-24 17:46:09 +0200

    P4PU-283 updated import

--- src/test/java/it/gov/pagopa/arc/connector/pullpayment/PullPaymentConnectorImplTest.java
+++ src/test/java/it/gov/pagopa/arc/connector/pullpayment/PullPaymentConnectorImplTest.java
@@ -5,7 +5,7 @@ import com.github.tomakehurst.wiremock.WireMockServer;
 import com.github.tomakehurst.wiremock.core.WireMockConfiguration;
 import it.gov.pagopa.arc.config.FeignConfig;
 import it.gov.pagopa.arc.connector.pullpayment.dto.PullPaymentNoticeDTO;
-import it.gov.pagopa.arc.fakers.connector.pullPayment.PullPaymentNoticeDTOFaker;
+import it.gov.pagopa.arc.fakers.pullPayment.PullPaymentNoticeDTOFaker;
 import it.gov.pagopa.arc.utils.MemoryAppender;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
