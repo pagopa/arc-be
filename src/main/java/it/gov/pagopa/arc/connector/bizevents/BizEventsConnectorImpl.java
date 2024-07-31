dd7d64f2ecdda454b34603b9e976ce61cb66e926
Giuseppe-La-Manna <Giuseppe.LaManna@emeal.nttdata.com>
2024-07-31 14:52:00 +0200

    [P4PU-289] updated BizEventsConnectorImpl

--- src/main/java/it/gov/pagopa/arc/connector/bizevents/BizEventsConnectorImpl.java
+++ src/main/java/it/gov/pagopa/arc/connector/bizevents/BizEventsConnectorImpl.java
@@ -6,6 +6,7 @@ import it.gov.pagopa.arc.connector.bizevents.dto.BizEventsTransactionsListDTO;
 import it.gov.pagopa.arc.exception.custom.BizEventsInvocationException;
 import it.gov.pagopa.arc.exception.custom.BizEventsReceiptNotFoundException;
 import it.gov.pagopa.arc.exception.custom.BizEventsTransactionNotFoundException;
+import it.gov.pagopa.arc.utils.Utilities;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.io.Resource;
@@ -45,6 +46,7 @@ public class BizEventsConnectorImpl implements BizEventsConnector {
                         e.status(),
                         e.getMessage());
             }else {
+                Utilities.logExceptionDetails(e);
                 throw new BizEventsInvocationException(ERROR_MESSAGE_INVOCATION_EXCEPTION);
             }
         }
@@ -60,6 +62,7 @@ public class BizEventsConnectorImpl implements BizEventsConnector {
             if(e.status() == HttpStatus.NOT_FOUND.value()){
                 throw new BizEventsTransactionNotFoundException("An error occurred handling request from biz-Events to retrieve transaction with transaction id [%s] for the current user".formatted(transactionId));
             }
+            Utilities.logExceptionDetails(e);
             throw new BizEventsInvocationException(ERROR_MESSAGE_INVOCATION_EXCEPTION);
         }
         return bizEventsTransactionDetailsDTO;
@@ -74,6 +77,7 @@ public class BizEventsConnectorImpl implements BizEventsConnector {
             if (e.status() == HttpStatus.NOT_FOUND.value()){
                 throw  new BizEventsReceiptNotFoundException("An error occurred handling request from biz-Events to retrieve transaction receipt with transaction id [%s] for the current user".formatted(transactionId));
             }
+            Utilities.logExceptionDetails(e);
             throw new BizEventsInvocationException(ERROR_MESSAGE_INVOCATION_EXCEPTION);
         }
         return transactionReceipt;
