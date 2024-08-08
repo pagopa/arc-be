1972774e95287e82306d12deb07da58f1a2e716c
Giuseppe-La-Manna <Giuseppe.LaManna@emeal.nttdata.com>
2024-08-08 14:51:18 +0200

    P4PU-361 added controller impl

---
+++ src/main/java/it/gov/pagopa/arc/controller/AssistanceControllerImpl.java
@@ -0,0 +1,25 @@
+package it.gov.pagopa.arc.controller;
+
+
+import it.gov.pagopa.arc.controller.generated.ArcZendeskAssistanceApi;
+import it.gov.pagopa.arc.model.generated.ZendeskAssistanceTokenResponse;
+import it.gov.pagopa.arc.service.ZendeskAssistanceTokenService;
+import jakarta.validation.constraints.NotNull;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class AssistanceControllerImpl implements ArcZendeskAssistanceApi {
+    private final ZendeskAssistanceTokenService zendeskAssistanceTokenService;
+
+    public AssistanceControllerImpl(ZendeskAssistanceTokenService zendeskAssistanceTokenService) {
+        this.zendeskAssistanceTokenService = zendeskAssistanceTokenService;
+    }
+
+    @Override
+    public ResponseEntity<ZendeskAssistanceTokenResponse> getZendeskAssistanceToken(@NotNull String userEmail) {
+        ZendeskAssistanceTokenResponse response = zendeskAssistanceTokenService.retrieveZendeskAssistanceTokenResponse(userEmail);
+        return new ResponseEntity<>(response,HttpStatus.OK);
+    }
+}
