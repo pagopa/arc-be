78178ebeee3c1ec6e3912d185c37538062da4504
oleksiybozhykntt <oleksiy.bozhyk@nttdata.com>
2024-08-09 16:38:42 +0200

    P4PU-385 removed unused code

--- src/test/java/it/gov/pagopa/arc/security/JwtAuthenticationFilterTest.java
+++ src/test/java/it/gov/pagopa/arc/security/JwtAuthenticationFilterTest.java
@@ -1,12 +1,8 @@
 package it.gov.pagopa.arc.security;
 
-import ch.qos.logback.classic.Level;
-import ch.qos.logback.classic.LoggerContext;
 import it.gov.pagopa.arc.dto.IamUserInfoDTO;
-import it.gov.pagopa.arc.exception.custom.InvalidTokenException;
 import it.gov.pagopa.arc.fakers.auth.IamUserInfoDTOFaker;
 import it.gov.pagopa.arc.service.TokenStoreService;
-import it.gov.pagopa.arc.utils.MemoryAppender;
 import jakarta.servlet.ServletException;
 import java.io.IOException;
 import java.util.Optional;
@@ -19,7 +15,6 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
-import org.slf4j.LoggerFactory;
 import org.springframework.context.annotation.Import;
 import org.springframework.mock.web.MockFilterChain;
 import org.springframework.mock.web.MockHttpServletRequest;
@@ -33,17 +28,10 @@ class JwtAuthenticationFilterTest {
   @InjectMocks
   private JwtAuthenticationFilter jwtAuthenticationFilter;
   private final String sampleJwt = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c";
-  private MemoryAppender memoryAppender;
 
   @BeforeEach
   public void setUp() {
     SecurityContextHolder.clearContext();
-    ch.qos.logback.classic.Logger logger = (ch.qos.logback.classic.Logger) LoggerFactory.getLogger("it.gov.pagopa.arc.security.JwtAuthenticationFilter");
-    memoryAppender = new MemoryAppender();
-    memoryAppender.setContext((LoggerContext) LoggerFactory.getILoggerFactory());
-    logger.setLevel(Level.ERROR);
-    logger.addAppender(memoryAppender);
-    memoryAppender.start();
   }
   @AfterEach
   public void clearContext() {
