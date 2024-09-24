8567d995f742bfaa952fd8b5095bb54c2c166265
Giuseppe-LaManna <127106061+Giuseppe-LaManna@users.noreply.github.com>
2024-09-24 09:00:50 +0200

    feat: [P4PU-000] Promote to UAT (#98)
    Co-authored-by: diegoitaliait <diego.lagosmorales@pagopa.it>
    Co-authored-by: oleksiybozhykntt <oleksiy.bozhyk@nttdata.com>
    Co-authored-by: Umberto Coppola Bottazzi <umberto.coppolabottazzi@pagopa.it>
    Co-authored-by: Diego Lagos <92735530+diegolagospagopa@users.noreply.github.com>

--- src/main/java/it/gov/pagopa/arc/service/AuthService.java
+++ src/main/java/it/gov/pagopa/arc/service/AuthService.java
@@ -1,10 +1,11 @@
 package it.gov.pagopa.arc.service;
 
+import it.gov.pagopa.arc.model.generated.TokenResponse;
 import it.gov.pagopa.arc.model.generated.UserInfo;
 
 public interface AuthService {
 
   UserInfo getUserLoginInfo();
 
-
+  TokenResponse generateAuthUser();
 }
