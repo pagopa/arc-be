c7c15b3c0ab5f7f04cb54b3fdd4367ca1ae4f672
Giuseppe-La-Manna <Giuseppe.LaManna@emeal.nttdata.com>
2024-09-26 14:59:04 +0200

    P4PU-507 updated FeignConfig

--- src/main/java/it/gov/pagopa/arc/config/FeignConfig.java
+++ src/main/java/it/gov/pagopa/arc/config/FeignConfig.java
@@ -1,6 +1,6 @@
 package it.gov.pagopa.arc.config;
 
-import it.gov.pagopa.arc.connector.bizevents.paid.BizEventsPaidNoticeRestClient;
+import it.gov.pagopa.arc.connector.bizevents.paidnotice.BizEventsPaidNoticeRestClient;
 import it.gov.pagopa.arc.connector.bizevents.BizEventsRestClient;
 import it.gov.pagopa.arc.connector.pullpayment.PullPaymentRestClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
