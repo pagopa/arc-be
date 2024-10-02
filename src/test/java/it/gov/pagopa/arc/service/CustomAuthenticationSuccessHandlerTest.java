294bae3bedeec6a6fb22c089c17eaca8662c336b
oleksiybozhykntt <oleksiy.bozhyk@nttdata.com>
2024-10-02 15:05:27 +0200

    updated test

--- src/test/java/it/gov/pagopa/arc/service/CustomAuthenticationSuccessHandlerTest.java
+++ src/test/java/it/gov/pagopa/arc/service/CustomAuthenticationSuccessHandlerTest.java
@@ -3,12 +3,14 @@ package it.gov.pagopa.arc.service;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
+import com.auth0.jwt.JWT;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import it.gov.pagopa.arc.config.JWTConfiguration;
 import it.gov.pagopa.arc.config.JWTSampleConfiguration;
 import it.gov.pagopa.arc.model.generated.TokenResponse;
 import java.io.IOException;
 import java.time.Instant;
+import java.util.List;
 import java.util.Map;
 import java.util.function.Consumer;
 import org.junit.jupiter.api.BeforeEach;
@@ -23,6 +25,8 @@ import org.springframework.security.oauth2.client.authentication.OAuth2Authentic
 import org.springframework.security.oauth2.core.oidc.OidcIdToken;
 import org.springframework.security.oauth2.core.oidc.user.DefaultOidcUser;
 import org.springframework.security.oauth2.core.user.OAuth2User;
+import wiremock.org.apache.hc.core5.http.ContentType;
+
 @ExtendWith(MockitoExtension.class)
 public class CustomAuthenticationSuccessHandlerTest {
 
@@ -38,47 +42,68 @@ public class CustomAuthenticationSuccessHandlerTest {
   void setUp(){
     tokenStoreService = new TokenStoreServiceImpl();
     JWTConfiguration jwtConfiguration = JWTSampleConfiguration.getCorrectConfiguration();
-    customAuthenticationSuccessHandler = new CustomAuthenticationSuccessHandler(new AccessTokenBuilderService(jwtConfiguration),new ObjectMapper(),jwtConfiguration,tokenStoreService,authService);
+    customAuthenticationSuccessHandler = new CustomAuthenticationSuccessHandler(
+        new AccessTokenBuilderService(jwtConfiguration),
+        new ObjectMapper(),
+        jwtConfiguration,
+        tokenStoreService,
+        authService);
   }
   @Test
   void givenAuthenticationRequestThenInResponseGetCustomTokenResponse() throws IOException {
-
-
-    String sampleIdToken = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6Ik1hcmNvIiwiaWF0IjoxNTE2MjM5MDIyLCJmaXNjYWxOdW1iZXIiOiI3ODkwMTIiLCJmYW1pbHlOYW1lIjoiUG9sbyIsImVtYWlsIjoibWFyY28ucG9sb0BleGFtcGxlLmNvbSIsImlzcyI6Imlzc3VlciJ9.AErwXpbHrT_0WvN86QuQ0nvnZndVxt8jnmiD1lfj1_A";
-    Consumer<Map<String, Object>> attributesConsumer = attributes -> {
-      attributes.putAll(Map.of(
-          "sub", "123456",
-          "fiscalNumber", "789012",
-          "familyName", "Polo",
-          "name", "Marco",
-          "email", "marco.polo@example.com",
-          "iss", "issuer"
-      ));
-    };
-    Mockito.when(customAuthenticationSuccessHandler.isInWhiteList("789012")).thenReturn(true);
-    Instant issuedAt = Instant.now();
-    Instant expiresAt = issuedAt.plusSeconds(3600);
-
-    OidcIdToken idToken = OidcIdToken.withTokenValue(sampleIdToken)
-        .issuedAt(issuedAt)
-        .expiresAt(expiresAt)
-        .claims(attributesConsumer)
-        .build();
-    OAuth2User principal = new DefaultOidcUser(null,idToken);
-    OAuth2AuthenticationToken oAuth2AuthenticationToken = new OAuth2AuthenticationToken(principal,null,"test-client");
+    Mockito.when(authService.getWhiteListUsers()).thenReturn(List.of("PLOMRC01P30L736Y"));
+    OAuth2AuthenticationToken oAuth2AuthenticationToken = getAuthenticationToken();
 
     MockHttpServletResponse response = new MockHttpServletResponse();
     MockHttpServletRequest request = new MockHttpServletRequest();
 
     customAuthenticationSuccessHandler.onAuthenticationSuccess(request,response,oAuth2AuthenticationToken);
 
-    assertEquals(response.getHeader("Content-Type"),"application/json");
+    assertEquals(ContentType.APPLICATION_JSON.getMimeType(),response.getContentType());
 
     TokenResponse token = new ObjectMapper().readValue(response.getContentAsString(),TokenResponse.class);
     assertNotNull(token.getAccessToken());
     assertNotNull(tokenStoreService.getUserInfo(token.getAccessToken()));
     assertNotNull(token.getTokenType());
     assertNotNull(token.getExpiresIn());
+  }
+
+  @Test
+  void givenAuthenticationRequestThenInResponseGetForbidden()
+      throws IOException {
+    Mockito.when(authService.getWhiteListUsers()).thenReturn(List.of("PLOMRC01P30L736YA","PLOMRC01P30L736"));
+    OAuth2AuthenticationToken oAuth2AuthenticationToken = getAuthenticationToken();
+
+    MockHttpServletResponse response = new MockHttpServletResponse();
+    MockHttpServletRequest request = new MockHttpServletRequest();
+
+    customAuthenticationSuccessHandler.onAuthenticationSuccess(request,response,oAuth2AuthenticationToken);
+
+    assertEquals(ContentType.APPLICATION_JSON.getMimeType(), response.getContentType());
+    assertEquals(403,response.getStatus());
+  }
+
+  private OAuth2AuthenticationToken getAuthenticationToken(){
+    Consumer<Map<String, Object>> attributesConsumer = attributes -> {
+      attributes.putAll(Map.of(
+          "sub", "123456",
+          "fiscalNumber", "TINIT-PLOMRC01P30L736Y",
+          "familyName", "Polo",
+          "name", "Marco",
+          "email", "marco.polo@example.com",
+          "iss", "issuer"
+      ));
+    };
+    Instant issuedAt = Instant.now();
+    Instant expiresAt = issuedAt.plusSeconds(3600);
+
+    OidcIdToken idToken = OidcIdToken.withTokenValue(JWT.create().toString())
+        .issuedAt(issuedAt)
+        .expiresAt(expiresAt)
+        .claims(attributesConsumer)
+        .build();
+    OAuth2User principal = new DefaultOidcUser(null,idToken);
+    return new OAuth2AuthenticationToken(principal,null,"test-client");
   }
 
 }
