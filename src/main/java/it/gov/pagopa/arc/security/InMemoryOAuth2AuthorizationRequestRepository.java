4b1e241c13e7d29ed272c188bca2941062606b25
oleksiybozhykntt <oleksiy.bozhyk@nttdata.com>
2024-10-22 16:53:33 +0200

    updated redis config and added caching for oauth2 state

--- src/main/java/it/gov/pagopa/arc/security/InMemoryOAuth2AuthorizationRequestRepository.java
+++ src/main/java/it/gov/pagopa/arc/security/InMemoryOAuth2AuthorizationRequestRepository.java
@@ -1,22 +1,17 @@
 package it.gov.pagopa.arc.security;
 
-import it.gov.pagopa.arc.config.RedisConfig;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-import lombok.extern.log4j.Log4j2;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.cache.annotation.CacheConfig;
 import org.springframework.security.oauth2.client.web.AuthorizationRequestRepository;
 import org.springframework.security.oauth2.core.endpoint.OAuth2AuthorizationRequest;
 import org.springframework.stereotype.Service;
 import org.springframework.util.StringUtils;
 
-@Log4j2
 @Service
 public class InMemoryOAuth2AuthorizationRequestRepository implements
     AuthorizationRequestRepository<OAuth2AuthorizationRequest> {
 
-  private OAuth2StateStoreRepository oAuth2AuthorizationRequest;
+  private final OAuth2StateStoreRepository oAuth2AuthorizationRequest;
   private static final String STATE = "state";
 
   public InMemoryOAuth2AuthorizationRequestRepository(OAuth2StateStoreRepository oAuth2AuthorizationRequest){
@@ -37,7 +32,6 @@ public class InMemoryOAuth2AuthorizationRequestRepository implements
     String state = authorizationRequest.getState();
     if (StringUtils.hasText(state)) {
       // Save the authorization request in the map using the state as the key
-      log.info("Saving authorization request for state: {}", state);
       oAuth2AuthorizationRequest.save(state, authorizationRequest);
     }
   }
@@ -52,7 +46,5 @@ public class InMemoryOAuth2AuthorizationRequestRepository implements
     }
     return null;
   }
-
-
 
 }
