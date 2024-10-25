cf4b2207aabe731ed351dc84399881cc4d6d65a9
diegoitaliait <diego.lagosmorales@pagopa.it>
2024-10-25 16:56:51 +0200

    github: added rulesets

--- .github/terraform/99_locals.tf
+++ .github/terraform/99_locals.tf
@@ -14,4 +14,37 @@ locals {
     "uat" : "uat"
     "prod" : "main"
   }
+
+  branches                  = ["develop", "uat"]
+  bypass_branch_rules_teams = ["p4pa-admins", "payments-cloud-admin"]
+
+  # this is use to lookup the id for each team
+  team_name_to_id = {
+    for team in data.github_organization_teams.all.teams :
+    team.name => team.id
+  }
+
+  branch_rulesets = {
+    develop = {
+      ref_name                        = "refs/heads/develop"
+      bypass_actors                   = false
+      required_linear_history         = true
+      require_code_owner_review       = false
+      required_approving_review_count = 0
+    }
+    uat = {
+      ref_name                        = "refs/heads/uat"
+      bypass_actors                   = false
+      required_linear_history         = false
+      require_code_owner_review       = false
+      required_approving_review_count = 1
+    },
+    main = {
+      ref_name                        = "refs/heads/main"
+      bypass_actors                   = false
+      required_linear_history         = false
+      require_code_owner_review       = true
+      required_approving_review_count = 0
+    },
+  }
 }
