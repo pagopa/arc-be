c6a4ba4715025b3159743e16adc5e4452dd4c3d1
Giuseppe-La-Manna <Giuseppe.LaManna@emeal.nttdata.com>
2024-11-06 17:04:52 +0100

    P4PU-538 updated tests

--- src/main/java/it/gov/pagopa/arc/service/TransactionsService.java
+++
@@ -1,8 +0,0 @@
-package it.gov.pagopa.arc.service;
-
-
-import it.gov.pagopa.arc.model.generated.TransactionsListDTO;
-
-public interface TransactionsService {
-    TransactionsListDTO retrieveTransactionsList(Integer page, Integer size, String filter);
-}
