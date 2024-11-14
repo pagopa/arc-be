992c814601b4564a6728b59143bcf855dedbc740
Giuseppe-La-Manna <Giuseppe.LaManna@emeal.nttdata.com>
2024-11-14 12:49:36 +0100

    P4PU-687 updated payment notices

--- src/main/java/it/gov/pagopa/arc/controller/PaymentNoticesControllerImpl.java
+++ src/main/java/it/gov/pagopa/arc/controller/PaymentNoticesControllerImpl.java
@@ -3,6 +3,7 @@ package it.gov.pagopa.arc.controller;
 import it.gov.pagopa.arc.controller.generated.ArcPaymentNoticesApi;
 import it.gov.pagopa.arc.model.generated.PaymentNoticesListDTO;
 import it.gov.pagopa.arc.service.PaymentNoticesService;
+import it.gov.pagopa.arc.utils.SecurityUtils;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RestController;
@@ -19,7 +20,9 @@ public class PaymentNoticesControllerImpl implements ArcPaymentNoticesApi {
 
     @Override
     public ResponseEntity<PaymentNoticesListDTO> getPaymentNotices(LocalDate dueDate, Integer size, Integer page) {
-        PaymentNoticesListDTO paymentNoticesListDTO = paymentNoticesService.retrievePaymentNotices(dueDate, size, page);
+        String userFiscalCode = SecurityUtils.getUserFiscalCode();
+        String userId = SecurityUtils.getUserId();
+        PaymentNoticesListDTO paymentNoticesListDTO = paymentNoticesService.retrievePaymentNotices(userId, userFiscalCode, dueDate, size, page);
         return new ResponseEntity<>(paymentNoticesListDTO, HttpStatus.OK);
     }
 }
