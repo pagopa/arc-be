b88b0fba7418107a3b92061f13a131dd57d55426
Giuseppe-La-Manna <Giuseppe.LaManna@emeal.nttdata.com>
2024-12-05 15:26:11 +0100

    P4PU-751 added DTO of GPD details

---
+++ src/main/java/it/gov/pagopa/arc/connector/gpd/enums/GPDPaymentNoticeDetailsStatus.java
@@ -0,0 +1,19 @@
+package it.gov.pagopa.arc.connector.gpd.enums;
+
+public enum GPDPaymentNoticeDetailsStatus {
+    DRAFT,
+
+    PUBLISHED,
+
+    VALID,
+
+    INVALID,
+
+    EXPIRED,
+
+    PARTIALLY_PAID,
+
+    PAID,
+
+    REPORTED
+}
