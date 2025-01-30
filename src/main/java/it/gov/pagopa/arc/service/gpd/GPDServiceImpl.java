d1451ce515c19d4fdf924737b79f97b430e02517
Giuseppe-La-Manna <Giuseppe.LaManna@emeal.nttdata.com>
2025-01-30 17:14:17 +0100

    P4PU-839 updated services

--- src/main/java/it/gov/pagopa/arc/service/gpd/GPDServiceImpl.java
+++ src/main/java/it/gov/pagopa/arc/service/gpd/GPDServiceImpl.java
@@ -2,23 +2,41 @@ package it.gov.pagopa.arc.service.gpd;
 
 import it.gov.pagopa.arc.connector.gpd.GPDConnector;
 import it.gov.pagopa.arc.connector.gpd.dto.GPDPaymentNoticeDetailsDTO;
+import it.gov.pagopa.arc.connector.gpd.dto.GPDPaymentNoticePayloadDTO;
+import it.gov.pagopa.arc.dto.IamUserInfoDTO;
 import it.gov.pagopa.arc.dto.mapper.gpd.GPDPaymentNoticeDetailsDTO2PaymentNoticeDetailsDTOMapper;
+import it.gov.pagopa.arc.dto.mapper.gpd.GPDPaymentNoticePayloadDTO2PaymentNoticeResponseDTOMapper;
+import it.gov.pagopa.arc.dto.mapper.gpd.PaymentNoticePayloadDTO2GPDPaymentNoticePayloadDTOMapper;
 import it.gov.pagopa.arc.model.generated.PaymentNoticeDetailsDTO;
+import it.gov.pagopa.arc.model.generated.PaymentNoticePayloadDTO;
+import it.gov.pagopa.arc.model.generated.PaymentNoticeResponseDTO;
 import org.springframework.stereotype.Service;
 
 @Service
 public class GPDServiceImpl implements GPDService{
     private final GPDConnector gpdConnector;
     private final GPDPaymentNoticeDetailsDTO2PaymentNoticeDetailsDTOMapper gpdPaymentNoticeDetailsDTO2PaymentNoticeDetailsDTOMapper;
+    private final PaymentNoticePayloadDTO2GPDPaymentNoticePayloadDTOMapper paymentNoticePayloadDTO2GPDPaymentNoticePayloadDTOMapper;
+    private final GPDPaymentNoticePayloadDTO2PaymentNoticeResponseDTOMapper paymentNoticePayloadDTO2PaymentNoticeResponseDTOMapper;
 
-    public GPDServiceImpl(GPDConnector gpdConnector, GPDPaymentNoticeDetailsDTO2PaymentNoticeDetailsDTOMapper gpdPaymentNoticeDetailsDTO2PaymentNoticeDetailsDTOMapper) {
+    public GPDServiceImpl(GPDConnector gpdConnector, GPDPaymentNoticeDetailsDTO2PaymentNoticeDetailsDTOMapper gpdPaymentNoticeDetailsDTO2PaymentNoticeDetailsDTOMapper, PaymentNoticePayloadDTO2GPDPaymentNoticePayloadDTOMapper paymentNoticePayloadDTO2GPDPaymentNoticePayloadDTOMapper, GPDPaymentNoticePayloadDTO2PaymentNoticeResponseDTOMapper paymentNoticePayloadDTO2PaymentNoticeResponseDTOMapper) {
         this.gpdConnector = gpdConnector;
         this.gpdPaymentNoticeDetailsDTO2PaymentNoticeDetailsDTOMapper = gpdPaymentNoticeDetailsDTO2PaymentNoticeDetailsDTOMapper;
+        this.paymentNoticePayloadDTO2GPDPaymentNoticePayloadDTOMapper = paymentNoticePayloadDTO2GPDPaymentNoticePayloadDTOMapper;
+        this.paymentNoticePayloadDTO2PaymentNoticeResponseDTOMapper = paymentNoticePayloadDTO2PaymentNoticeResponseDTOMapper;
     }
 
     @Override
     public PaymentNoticeDetailsDTO retrievePaymentNoticeDetailsFromGPD(String userId, String organizationFiscalCode, String iupd) {
         GPDPaymentNoticeDetailsDTO gpdPaymentNoticeDetails = gpdConnector.getPaymentNoticeDetails(userId, organizationFiscalCode, iupd);
         return gpdPaymentNoticeDetailsDTO2PaymentNoticeDetailsDTOMapper.toPaymentNoticeDetailsDTO(gpdPaymentNoticeDetails);
+    }
+
+    @Override
+    public PaymentNoticeResponseDTO generatePaymentNoticeFromGPD(IamUserInfoDTO iamUserInfoDTO, PaymentNoticePayloadDTO paymentNoticePayloadDTO) {
+        GPDPaymentNoticePayloadDTO gpdPaymentNoticePayloadDTO = paymentNoticePayloadDTO2GPDPaymentNoticePayloadDTOMapper.map(iamUserInfoDTO, paymentNoticePayloadDTO);
+        GPDPaymentNoticePayloadDTO gpdPaymentNoticeResponse = gpdConnector.generatePaymentNotice(paymentNoticePayloadDTO.getPaTaxCode(), gpdPaymentNoticePayloadDTO);
+
+        return paymentNoticePayloadDTO2PaymentNoticeResponseDTOMapper.map(gpdPaymentNoticeResponse);
     }
 }
