d4f28fa3f503b96fe495a5279153559a696ff396
antonioT90 <34568575+antonioT90@users.noreply.github.com>
2025-10-08 11:50:19 +0200

    chore: Removed integration towards p4pa subscription (#216)

--- .github/terraform/99_variables.tf
+++ .github/terraform/99_variables.tf
@@ -38,6 +38,6 @@ variable "github_repository_environment" {
   default = {
     protected_branches     = false
     custom_branch_policies = true
-    reviewers_teams        = ["p4pa-admins"]
+    reviewers_teams        = ["arc-admins"]
   }
 }
