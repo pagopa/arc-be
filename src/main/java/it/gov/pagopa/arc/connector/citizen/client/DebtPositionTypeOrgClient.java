98e54cc45c7cfad5ffc2847f9b96fe83360b854e
Giuseppe-LaManna <127106061+Giuseppe-LaManna@users.noreply.github.com>
2025-10-23 15:50:24 +0200

    feat: [P4PU-994] added debtPositionTypeOrgDetails API (#10)

--- src/main/java/it/gov/pagopa/arc/connector/citizen/client/DebtPositionTypeOrgClient.java
+++ src/main/java/it/gov/pagopa/arc/connector/citizen/client/DebtPositionTypeOrgClient.java
@@ -2,10 +2,14 @@ package it.gov.pagopa.arc.connector.citizen.client;
 
 import it.gov.pagopa.arc.connector.citizen.config.CitizenApisHolder;
 import it.gov.pagopa.pu.citizen.dto.generated.DebtPositionTypeOrgsWithSpontaneousDTO;
+import it.gov.pagopa.pu.citizen.dto.generated.DebtPositionTypeOrgsWithSpontaneousDetailsDTO;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.HttpClientErrorException;
 
 import java.util.List;
 
+@Slf4j
 @Service
 public class DebtPositionTypeOrgClient {
 
@@ -18,5 +22,14 @@ public class DebtPositionTypeOrgClient {
     public List<DebtPositionTypeOrgsWithSpontaneousDTO> getDebtPositionTypeOrgsWithSpontaneous(Long organizationId, String accessToken){
         return citizenApisHolder.getDebtPositionTypeOrgApi(accessToken)
                 .getDebtPositionTypeOrgsWithSpontaneous(organizationId);
+    }
+
+    public DebtPositionTypeOrgsWithSpontaneousDetailsDTO getDebtPositionTypeOrgsWithSpontaneousDetail(Long brokerId, Long organizationId, Long debtPositionTypeOrgId, String accessToken){
+        try {
+            return citizenApisHolder.getDebtPositionTypeOrgApi(accessToken).getDebtPositionTypeOrgsWithSpontaneousDetail(brokerId, organizationId, debtPositionTypeOrgId);
+        }catch (HttpClientErrorException.NotFound e) {
+            log.warn("DebtPositionTypeOrgsWithSpontaneousDetails with debtPositionTypeOrgId {} not found", debtPositionTypeOrgId);
+            return null;
+        }
     }
 }
--- src/test/java/it/gov/pagopa/arc/connector/citizen/client/DebtPositionTypeOrgClientTest.java
+++ src/test/java/it/gov/pagopa/arc/connector/citizen/client/DebtPositionTypeOrgClientTest.java
@@ -4,6 +4,7 @@ import it.gov.pagopa.arc.connector.citizen.config.CitizenApisHolder;
 import it.gov.pagopa.arc.utils.TestUtils;
 import it.gov.pagopa.pu.citizen.controller.generated.DebtPositionTypeOrgApi;
 import it.gov.pagopa.pu.citizen.dto.generated.DebtPositionTypeOrgsWithSpontaneousDTO;
+import it.gov.pagopa.pu.citizen.dto.generated.DebtPositionTypeOrgsWithSpontaneousDetailsDTO;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -11,12 +12,13 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.client.HttpClientErrorException;
 import uk.co.jemos.podam.api.PodamFactory;
 
 import java.util.List;
 
-import static org.junit.jupiter.api.Assertions.assertNotNull;
-import static org.junit.jupiter.api.Assertions.assertSame;
+import static org.junit.jupiter.api.Assertions.*;
 
 @ExtendWith(MockitoExtension.class)
 class DebtPositionTypeOrgClientTest {
@@ -57,5 +59,41 @@ class DebtPositionTypeOrgClientTest {
         //then
         assertNotNull(result);
         assertSame(expectedResult, result);
+    }
+
+    @Test
+    void givenOrganizationIdAndDebtPositionTypeOrgIdWhenGetDebtPositionTypeOrgsWithSpontaneousDetailThenReturnDebtPositionTypeOrgsWithSpontaneousDetailsDTO() {
+        //given
+        String accessToken = "accessToken";
+        Long organizationId = 1L;
+        Long debtPositionTypeOrgId = 1L;
+        Long brokerId = 1L;
+
+        DebtPositionTypeOrgsWithSpontaneousDetailsDTO expectedResult = podamFactory.manufacturePojo(DebtPositionTypeOrgsWithSpontaneousDetailsDTO.class);
+
+        Mockito.when(citizenApisHolderMock.getDebtPositionTypeOrgApi(accessToken)).thenReturn(debtPositionTypeOrgApiMock);
+        Mockito.when(debtPositionTypeOrgApiMock.getDebtPositionTypeOrgsWithSpontaneousDetail(brokerId, organizationId, debtPositionTypeOrgId)).thenReturn(expectedResult);
+        //when
+        DebtPositionTypeOrgsWithSpontaneousDetailsDTO result = debtPositionTypeOrgClient.getDebtPositionTypeOrgsWithSpontaneousDetail(brokerId, organizationId, debtPositionTypeOrgId, accessToken);
+        //then
+        assertNotNull(result);
+        assertSame(expectedResult, result);
+    }
+
+    @Test
+    void givenExceptionWhenGetDebtPositionTypeOrgsWithSpontaneousDetailThenReturnNull() {
+        String accessToken = "accessToken";
+        Long organizationId = 1L;
+        Long debtPositionTypeOrgId = 1L;
+        Long brokerId = 1L;
+
+        Mockito.when(citizenApisHolderMock.getDebtPositionTypeOrgApi(accessToken)).thenReturn(debtPositionTypeOrgApiMock);
+
+        Mockito.when(debtPositionTypeOrgApiMock.getDebtPositionTypeOrgsWithSpontaneousDetail(brokerId, organizationId, debtPositionTypeOrgId))
+                .thenThrow(HttpClientErrorException.create(HttpStatus.NOT_FOUND, "NotFound", null, null, null));
+
+        DebtPositionTypeOrgsWithSpontaneousDetailsDTO result = debtPositionTypeOrgClient.getDebtPositionTypeOrgsWithSpontaneousDetail(brokerId, organizationId, debtPositionTypeOrgId, accessToken);
+
+        assertNull(result);
     }
 }
