301d8d632c56065b7b881cc763ec6028128d95ab
Giuseppe-LaManna <127106061+Giuseppe-LaManna@users.noreply.github.com>
2025-12-12 16:36:53 +0100

    feat: P4PU-1076 added business logic to retrieve debtor installment api (#32)

--- build.gradle.kts
+++ build.gradle.kts
@@ -199,7 +199,8 @@ openApiGenerate {
         "BrokerInfoDTO" to "it.gov.pagopa.pu.citizen.dto.generated.BrokerInfoDTO",
 		"PagedDebtorDebtPositionDTO" to "it.gov.pagopa.pu.citizen.dto.generated.PagedDebtorDebtPositionDTO",
 		"DebtorUnpaidDebtPositionOverviewDTO" to "it.gov.pagopa.pu.citizen.dto.generated.DebtorUnpaidDebtPositionOverviewDTO",
-		"InstallmentDebtorExtendedDTO" to "it.gov.pagopa.pu.citizen.dto.generated.InstallmentDebtorExtendedDTO"
+		"InstallmentDebtorExtendedDTO" to "it.gov.pagopa.pu.citizen.dto.generated.InstallmentDebtorExtendedDTO",
+		"DebtorUnpaidDebtPositionInstallmentsDTO" to "it.gov.pagopa.pu.citizen.dto.generated.DebtorUnpaidDebtPositionInstallmentsDTO"
 
 	))
 }
