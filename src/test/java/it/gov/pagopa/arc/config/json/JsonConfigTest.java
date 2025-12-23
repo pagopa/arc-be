813fbfb4ee0059090806ffa2d0f26557f6077ea2
antonio.torre <antonio.torre@pagopa.it>
2025-12-23 10:19:45 +0100

    chore: Added test on jackson3 deserialization

--- src/test/java/it/gov/pagopa/arc/config/json/JsonConfigTest.java
+++ src/test/java/it/gov/pagopa/arc/config/json/JsonConfigTest.java
@@ -1,10 +1,12 @@
 package it.gov.pagopa.arc.config.json;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonPropertyOrder;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import it.gov.pagopa.arc.utils.TestUtils;
+import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.junit.jupiter.api.Assertions;
@@ -23,52 +25,60 @@ class JsonConfigTest {
   private final ObjectMapper j2ObjectMapper = jsonConfig.objectMapper();
   private final JsonMapper j3JsonMapper = jsonConfig.objectMapperJackson3();
 
-  @Data
-  @NoArgsConstructor
-  @JsonPropertyOrder({"name", "nullField", "nonNullableNullField", "value", "dateTime", "offsetDateTime"})
-  public static class SampleDTO {
-    public String name;
-    private String nullField;
-    @JsonInclude(JsonInclude.Include.ALWAYS)
-    private String nonNullableNullField;
-    public Integer value;
-    public LocalDateTime dateTime;
-    public OffsetDateTime offsetDateTime;
-  }
+    @Data
+    @NoArgsConstructor(onConstructor_ = @JsonCreator)
+    @AllArgsConstructor
+    @JsonPropertyOrder({"name", "nullField", "nonNullableNullField", "value", "dateTime", "offsetDateTime", "implicitField"})
+    public static class SampleDTO {
+        private String name;
+        private String nullField;
+        @JsonInclude(JsonInclude.Include.ALWAYS)
+        private String nonNullableNullField;
+        private Integer value;
+        private LocalDateTime dateTime;
+        private OffsetDateTime offsetDateTime;
+        private String implicitField;
+
+        public void setName(String name) {
+            this.implicitField = name;
+            this.name = name;
+        }
+
+    }
 
   @BeforeEach
   void init(){
       TestUtils.clearDefaultTimezone();
   }
 
-  @Test
-  void testJackson2Jackson3ConfigurationAlignment() throws JsonProcessingException {
-    // Given
-    SampleDTO dto = new SampleDTO();
-    dto.setName("NAME");
-    dto.setValue(42);
-    dto.setDateTime(LocalDateTime.now());
-    dto.setOffsetDateTime(OffsetDateTime.now());
+    @Test
+    void testJackson2Jackson3ConfigurationAlignment() throws JsonProcessingException {
+        // Given
+        SampleDTO dto = new SampleDTO();
+        dto.setName("NAME");
+        dto.setValue(42);
+        dto.setDateTime(LocalDateTime.now());
+        dto.setOffsetDateTime(OffsetDateTime.now());
 
-    // When
-    String j2Serialized = j2ObjectMapper.writeValueAsString(dto);
-    String j3Serialized = j3JsonMapper.writeValueAsString(dto);
+        // When
+        String j2Serialized = j2ObjectMapper.writeValueAsString(dto);
+        String j3Serialized = j3JsonMapper.writeValueAsString(dto);
 
-    // Then
-    Assertions.assertEquals(j2Serialized, j3Serialized);
+        // Then
+        Assertions.assertEquals(j2Serialized, j3Serialized);
 
-    // When deserialized
-    SampleDTO j2Deserialized = j2ObjectMapper.readValue(j2Serialized, SampleDTO.class);
-    SampleDTO j3Deserialized = j3JsonMapper.readValue(j2Serialized, SampleDTO.class);
+        // When deserialized
+        SampleDTO j2Deserialized = j2ObjectMapper.readValue(j2Serialized, SampleDTO.class);
+        SampleDTO j3Deserialized = j3JsonMapper.readValue(j2Serialized, SampleDTO.class);
 
-    Assertions.assertEquals(dto, j2Deserialized);
-    Assertions.assertEquals(dto, j3Deserialized);
-  }
+        Assertions.assertEquals(dto, j2Deserialized);
+        Assertions.assertEquals(dto, j3Deserialized);
+    }
 
-  @Test
-  void testDateConversions() throws JsonProcessingException {
-    // Given
-    String json = """
+    @Test
+    void testDateConversionsAndImplicitSet() throws JsonProcessingException {
+        // Given
+        String json = """
       {
           "dateTime": "2025-12-22T17:17:39.940891+00:00",
           "name": "NAME",
@@ -78,18 +88,19 @@ class JsonConfigTest {
       }
       """;
 
-    SampleDTO expectedResult = new SampleDTO();
-    expectedResult.setName("NAME");
-    expectedResult.setValue(42);
-    expectedResult.setDateTime(LocalDateTime.of(2025, 12, 22, 18, 17, 39, 940891000));
-    expectedResult.setOffsetDateTime(OffsetDateTime.of(2025, 12, 22, 18, 17, 39, 941234000, ZoneOffset.of("+01:00")));
+        SampleDTO expectedResult = new SampleDTO();
+        expectedResult.setName("NAME");
+        expectedResult.setValue(42);
+        expectedResult.setDateTime(LocalDateTime.of(2025, 12, 22, 18, 17, 39, 940891000));
+        expectedResult.setOffsetDateTime(OffsetDateTime.of(2025, 12, 22, 18, 17, 39, 941234000, ZoneOffset.of("+01:00")));
+        expectedResult.setImplicitField(expectedResult.getName());
 
-    // When
-    SampleDTO j2Deserialized = j2ObjectMapper.readValue(json, SampleDTO.class);
-    SampleDTO j3Deserialized = j3JsonMapper.readValue(json, SampleDTO.class);
+        // When
+        SampleDTO j2Deserialized = j2ObjectMapper.readValue(json, SampleDTO.class);
+        SampleDTO j3Deserialized = j3JsonMapper.readValue(json, SampleDTO.class);
 
-    // Then
-    Assertions.assertEquals(expectedResult, j2Deserialized);
-    Assertions.assertEquals(expectedResult, j3Deserialized);
-  }
+        // Then
+        Assertions.assertEquals(expectedResult, j2Deserialized);
+        Assertions.assertEquals(expectedResult, j3Deserialized);
+    }
 }
